31455a3c2df5ed28fe96487a55fd8b985613fe4b
Jieun Han <yjyyls66@naver.com>
2023-05-31 19:18:38 +0900

    feat : login setting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import NewProduct from "./pages/NewProduct";
+import ProductDetail from "./pages/ProductDetail";
+import MyCart from "./pages/MyCart";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <NotFound />,
+        children: [
+            { index: true, path: "/", element: <Home /> },
+            { path: "/products", element: <AllProducts /> },
+            {
+                path: "/products/new",
+                element: <NewProduct />,
+            },
+            {
+                path: "/products/:id",
+                element: <ProductDetail />,
+            },
+            {
+                path: "/carts",
+                element: <MyCart />,
+            },
+        ],
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/MyCart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MyCart() {
+  return (
+    <div>MyCart</div>
+  )
+}
+
+export default MyCart
